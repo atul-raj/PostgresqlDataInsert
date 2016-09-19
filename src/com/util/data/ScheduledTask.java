d37cd470b7321f45184b6534467eeb5956da0e3d
unknown <AR105487@SATHST-PCS07087.gegdc.ad>
2016-09-19 16:02:23 +0530

    After Random change

--- src/com/util/data/ScheduledTask.java
+++ src/com/util/data/ScheduledTask.java
@@ -1,4 +1,5 @@
 package com.util.data;
+
 import java.util.TimerTask;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -12,100 +13,107 @@ import java.util.Random;
 // Create a class extends with TimerTask
 public class ScheduledTask extends TimerTask {
 
-public void run() {
+	public void run() {
 
-String turbine = "";
-String site = "";
-String customer = "";
-String project = "";
+		String turbine = "";
+		String site = "";
+		String customer = "";
+		String project = "";
 
-Random random1 = new Random();
+		Random random1 = new Random();
 
-for (int count = 1; count <= 4; count++) {
+		for (int count = 1; count <= 4; count++) {
 
-         site = "S" + count ;
-         switch (count) {
-           case 1:  project = "US-North";
-                    customer = "GE-ELECTRIC";
-                    for(int i = 1 ; i<= 4; i++){
-                    	 turbine = "T" + i ;
-                    	 int status = random1.nextInt(8);
-                    	 generateOtherData(turbine, site, customer, project, status);
-                    	 
-                    }
-                    break;
-           case 2:  project = "US-South";
-                    customer = "DTE Energy";
-                    for(int i = 1 ; i<= 4; i++){
-                   	 turbine = "T" + i ;
-                   	int status = random1.nextInt(8);
-                   	 generateOtherData(turbine, site, customer, project,status);
-                   	 
-                   }
-                    break;
-           case 3:  project = "US-East";
-                    customer = "Public Service Elec & Gas";  
-                    for(int i = 1 ; i<= 3; i++){
-                   	 turbine = "T" + i ;
-                     int status = random1.nextInt(8);
-                   	 generateOtherData(turbine, site, customer, project,status);
-                   	 
-                   }
-                    break;
-           case 4:  project = "US-West";
-                    customer = "Duke Energy Carolinas";
-                    for(int i = 1 ; i<= 5; i++){
-                   	 turbine = "T" + i ;
-                     int status = random1.nextInt(8);
-                   	 generateOtherData(turbine, site, customer, project,status);
-                   	 
-                   }
-                    break;
-             }  
-       
-         
-    
+			site = "S" + count;
+			switch (count) {
+			case 1:
+				project = "US-North";
+				customer = "GE-ELECTRIC";
+				for (int i = 1; i <= 4; i++) {
+					turbine = "T" + i;
+					int status = random1.nextInt(8);
+					generateOtherData(turbine, site, customer, project, status);
 
-     }
-     }
-public void generateOtherData(String turbine,String site,String customer,String project,int status1){
-	String parameters = "";
+				}
+				break;
+			case 2:
+				project = "US-South";
+				customer = "DTE Energy";
+				for (int i = 1; i <= 4; i++) {
+					turbine = "T" + i;
+					int status = random1.nextInt(8);
+					generateOtherData(turbine, site, customer, project, status);
+
+				}
+				break;
+			case 3:
+				project = "US-East";
+				customer = "Public Service Elec & Gas";
+				for (int i = 1; i <= 3; i++) {
+					turbine = "T" + i;
+					int status = random1.nextInt(8);
+					generateOtherData(turbine, site, customer, project, status);
+
+				}
+				break;
+			case 4:
+				project = "US-West";
+				customer = "Duke Energy Carolinas";
+				for (int i = 1; i <= 5; i++) {
+					turbine = "T" + i;
+					int status = random1.nextInt(8);
+					generateOtherData(turbine, site, customer, project, status);
+
+				}
+				break;
+			}
+
+		}
+	}
+
+	public void generateOtherData(String turbine, String site, String customer,
+			String project, int status1) {
+		String parameters = "";
+
+		String status = "";
+		double voltage = 0.0;
+		double temperature = 0.0;
+		double rangeMin = 350;
+		double rangeMax = 600;
+		double rangeMinV = 5;
+		double rangeMaxV = 25;
+		DecimalFormat df = new DecimalFormat("#.##");
 	
-	String status = "" ;
-	double voltage = 0.0;
-	double temperature =  0.0;
-	double rangeMin = 350;
-	double rangeMax = 600;
-	double rangeMinV = 5;
-	double rangeMaxV = 25;
-	DecimalFormat df = new DecimalFormat("#.##"); 
-	DataGen dGen = new DataGen();
-    temperature = ThreadLocalRandom.current().nextDouble(rangeMin, rangeMax);
+		temperature = ThreadLocalRandom.current()
+				.nextDouble(rangeMin, rangeMax);
 
-temperature = Double.valueOf(df.format(temperature));
-voltage = ThreadLocalRandom.current().nextDouble(rangeMinV, rangeMaxV);
+		temperature = Double.valueOf(df.format(temperature));
+		voltage = ThreadLocalRandom.current().nextDouble(rangeMinV, rangeMaxV);
 
-voltage = Double.valueOf(df.format(voltage));
-   
-     if (status1 == 0){
-         status = "0";
-         voltage = 0.0;
-          }else {
-          status = "1";
-          }
-      
-       try {
-          parameters = "{\"turbine\":\""+turbine+"\",\"site\":\""+site+"\",\"customer\":\""+customer+"\",\"project\":\""+project+"\", \"status\":"+status+", \"temperature\":"+temperature+", \"voltage\":"+voltage+"} ";
-        // System.out.println(parameters);
-        
- StringEntity params =new StringEntity(parameters);
+		voltage = Double.valueOf(df.format(voltage));
 
- dGen.insertData(params);
+		if (status1 == 0) {
+			status = "0";
+			voltage = 0.0;
+		} else {
+			status = "1";
+		}
 
- } catch (UnsupportedEncodingException e1) {
- // TODO Auto-generated catch block
- // e1.printStackTrace();
- } 
+		try {
+			parameters = "{\"turbine\":\"" + turbine + "\",\"site\":\"" + site
+					+ "\",\"customer\":\"" + customer + "\",\"project\":\""
+					+ project + "\", \"status\":" + status
+					+ ", \"temperature\":" + temperature + ", \"voltage\":"
+					+ voltage + "} ";
+			// System.out.println(parameters);
+
+			StringEntity params = new StringEntity(parameters);
+			DataGen dGen = new DataGen();
+			dGen.insertData(params);
+			} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			// e1.printStackTrace();
+		}
+	}
+
 }
-
-     }           
